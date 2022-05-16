289b0f1fb30174fa009b06674dcf3a05613ec702
tharodon <tharodon>
2022-05-16 20:38:50 +0300

    add message in messages_US.properties

--- src/main/java/com/example/employeesoap/exception/JsonParsingException.java
+++ src/main/java/com/example/employeesoap/exception/JsonParsingException.java
@@ -1,9 +1,14 @@
 package com.example.employeesoap.exception;
 
+import static com.example.employeesoap.support.ConstantsSupport.*;
+
 import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class JsonParsingException extends RuntimeException{
     public JsonParsingException(String jsonObj, String message) {
-        super(MessageFormat.format("Failed parsing json, JsonObject: {0}. Error: {1}", jsonObj, message));
+        super(MessageFormat.format(ResourceBundle.getBundle(
+                FILENAME, new Locale("US", "US")).getString(JSON_BUNDLE_KEY), jsonObj, message));
     }
 }
--- src/main/java/com/example/employeesoap/support/ConstantsSupport.java
+++ src/main/java/com/example/employeesoap/support/ConstantsSupport.java
@@ -13,4 +13,5 @@ public class ConstantsSupport {
     public static final String AGE_BUNDLE_KEY = "invalid.age";
     public static final String SALARY_BUNDLE_KEY = "invalid.salary";
     public static final String TASKS_BUNDLE_KEY = "invalid.tasks";
+    public static final String JSON_BUNDLE_KEY = "json.failed";
 }
